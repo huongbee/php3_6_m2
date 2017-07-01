de8de3c3382d5dd1a20686d540f9dd560b5683d7
huong huong <huongnguyen08.cv@gmail.com>
2017-07-01 19:30:05 +0700

    hàm

---
+++ bai12.php
@@ -0,0 +1,15 @@
+<?php
+//echo rand(11,20); //[1->10]
+$c = 'Chào cả lớp';
+// echo $c = str_replace( 'cả lớp','các bạn', $c);
+
+// echo substr($c, 6);
+
+
+var_dump(checkdate(2,32,2017));
+date_default_timezone_set('Asia/Ho_chi_minh');
+
+echo date('Y-m-d H:i:s');
+
+print_r(getdate());
+?>
